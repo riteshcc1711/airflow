587e31b71dbb7aff8fd0f7c9ed3ed3bc647e171f
riteshcc1711 <97429071+riteshcc1711@users.noreply.github.com>
2022-02-10 01:01:26 +0530

    Add files via upload

---
+++ POC-AIRFLOW_v2/project/dags/pyspark_dag.py
@@ -0,0 +1,31 @@
+import airflow
+import time
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.contrib.operators.databricks_operator import DatabricksSubmitRunOperator
+from airflow.models import Variable
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'start_date': airflow.utils.dates.days_ago(2)
+}
+
+with DAG(dag_id='Airflow_Databricks_Integration_with_cluster_creation', default_args=default_args, schedule_interval='@daily') as dag:
+    notebook_task_params = {
+            # 'new_cluster': new_cluster,
+            'existing_cluster_id': '0201-113152-gh78wyan',
+            'notebook_task': {
+                'notebook_path': '/Users/ritesh.d.jain@accenture.com/test',
+            }
+        }
+
+    notebook_task = DatabricksSubmitRunOperator(
+            task_id='Airflow_Databricks_Integration_with_cluster_creation',
+            databricks_conn_id='databricks_default',
+            dag=dag,
+            json=notebook_task_params)
+
